35aa5c06ccd20efcb602d27a6266e5b4b1492764
sakainewman <c1032319@newcastle.ac.uk>
2024-04-28 18:01:52 +0100

    Added Error 403 Page

---
+++ client/src/pages/Error/Error403.js
@@ -0,0 +1,11 @@
+import Header from "../../components/Header"
+
+
+export default function Error403() {
+    return (
+        <>
+            <Header />
+            <h1>Error 403: Page Forbidden</h1>
+        </>
+    )
+}
